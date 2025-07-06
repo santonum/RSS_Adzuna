e410a37c69a13495d554665f8c87e8d9a56b282a
santonum <m.github@santonum.eu>
2025-07-06 22:26:20 +0200

    Initial commit for Adzuna RSS

---
+++ rss_server.py
@@ -0,0 +1,42 @@
+from flask import Flask, Response
+import requests
+from feedgen.feed import FeedGenerator
+
+app = Flask(__name__)
+
+@app.route("/rss")
+def rss_feed():
+    # Appel API Adzuna
+    app_id = "TON_APP_ID"
+    app_key = "TA_CLE_API"
+    url = "https://api.adzuna.com/v1/api/jobs/fr/search/1"
+    params = {
+        "app_id": app_id,
+        "app_key": app_key,
+        "what": "bts electrotechnique",
+        "where": "Charente-Maritime",
+        "results_per_page": 10
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    # Génération du flux RSS
+    fg = FeedGenerator()
+    fg.title("Offres Adzuna – BTS Electrotechnique en Charente-Maritime")
+    fg.link(href="https://www.adzuna.fr")
+    fg.description("Flux RSS basé sur l'API Adzuna")
+
+    for job in data["results"]:
+        fe = fg.add_entry()
+        fe.title(job["title"])
+        fe.link(href=job["redirect_url"])
+        fe.description(job.get("description", "")[:300] + "…")
+
+    rss_xml = fg.rss_str(pretty=True)
+
+    # Retourner la réponse HTTP avec le bon content-type
+    return Response(rss_xml, mimetype='application/rss+xml')
+
+if __name__ == "__main__":
+    app.run(debug=True)
